2e6046a5ca66883118fcabfb6ad55a3c7663cbfb
Anushastha <shresthanu123@gmail.com>
2021-12-06 12:32:35 +0545

    initial commit

--- Day 3/Lab8.py
+++ Day 3/Lab8.py
@@ -1,1 +1,9 @@
 #8)Given a n-digit number. Find the sum of its digits.
+
+n=int(input("Enter a number= "))
+total=0
+while(n>0):
+    digit=n%10
+    total=total+digit
+    n=n//10
+print("the total sum of digits is:",(total))
---
+++ Day 3/Lab9.py
@@ -0,0 +1,17 @@
+#Take username and password from user and check it again for the three times whether the user has entered the right
+#username and password. If yes then print a message "Logged in Successfully", if not then print invalid credentials
+#for consecutive 3 times and if the limit exceeds than print "Attempt finished".
+username=input('enter your username= ')
+password=input('enter your password= ')
+for i in range(0,3):
+    print("enter your credentials")
+    username1=input("Enter your username:")
+    password1=input("Enter your password:")
+    if username==username1 and password==password1:
+        print("you are successfully logged in.")
+        break
+    else:
+        if (username!=username1 or password!=password1):
+            print("Invalid credentials. Try again.")
+else:
+    print("3 attempts finished")
